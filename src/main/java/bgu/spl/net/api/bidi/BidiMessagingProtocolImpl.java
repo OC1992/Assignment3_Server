c40dacb52d4ffdd127e4ae21b1a9c120e7ea97fc
ofirx <ofirx@post.bgu.ac.il>
2018-12-26 16:32:35 +0200

    7

--- src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocolImpl.java
+++ src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocolImpl.java
@@ -1,9 +1,8 @@
 package bgu.spl.net.api.bidi;
 
 import bgu.spl.net.srv.BlockingConnectionHandler;
-import bgu.spl.net.srv.ConnectionHandler;
 
-import java.io.IOException;
+import java.util.LinkedList;
 
 public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol<String>
 {
@@ -11,6 +10,7 @@ public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol<Strin
     private boolean TPC=false;
     private int myName;
     private Connections<String> connections;
+    private LinkedList<String> listOfUsers=new LinkedList<String>();
 
 
     @Override
@@ -29,14 +29,42 @@ public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol<Strin
     @Override
     public void process(String message) {
         int ind = -1;
-        if (message!=null)
-             ind=message.indexOf(' ');
+        if (message != null)
+            ind = message.indexOf(' ');
         assert message != null;
-        String op=message.substring(0,ind);
+        String op = message.substring(0, ind);
+        String res=message.substring(ind);
+        switch (op) {
+            case "REGISTER":
+                int ind2 = -1;
+                ind2=res.indexOf(' ');
+                if (!listOfUsers.contains(res.substring(ind,ind2)))
+                    connections.send(myName,"ERROR");
+                else listOfUsers.add(res.substring(ind,ind2));
+
+            case "LOGIN":
+
+            case "LOGOUT":
+
+            case "FOLLOW":
+
+            case "POST":
+
+            case "PM":
+
+            case "USERLIST":
+
+            case "STAT":
+
+            case "NOTIFICATION":
+
+            case "ACK":
+
+            case "ERROR":
 
 
 
-
+        }
     }
 
     @Override
